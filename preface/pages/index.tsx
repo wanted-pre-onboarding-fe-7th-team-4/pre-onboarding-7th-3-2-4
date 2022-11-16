ab87a5f14312fa48055ecfaf59f66c8432c16c6e
movie42 <movie3342@gmail.com>
2022-11-16 17:12:58 +0900

    login api 구현

--- preface/pages/index.tsx
+++ preface/pages/index.tsx
@@ -3,9 +3,13 @@ import React, { useEffect } from "react";
 
 const Home = () => {
   const request = async () => {
-    const response = await axios.post("/api/login?email='hi@naver.com'");
+    const response = await axios.post("/api/login", {
+      email: "test@gmail.com",
+      password: "123qwe!@"
+    });
+
     console.log(response.data);
-    return response.data;
+    return response;
   };
 
   useEffect(() => {
