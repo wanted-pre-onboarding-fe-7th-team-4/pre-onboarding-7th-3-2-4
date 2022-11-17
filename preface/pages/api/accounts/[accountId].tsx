0ef675c3dafcf20d89f09842021cd560f53a6950
ganeodolu <ganeodolu@gmail.com>
2022-11-17 16:02:47 +0900

    feat: accout get API 구현

--- preface/pages/api/accounts/[accountId].tsx
+++ preface/pages/api/accounts/[accountId].tsx
@@ -2,7 +2,7 @@ import axios, { AxiosError } from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import CookieService from "service/CookieService";
 
-export default async function userHandler(req: NextApiRequest, res: NextApiResponse) {
+export default async function accountsByIdHandler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const {
       query: { accountId },
@@ -13,6 +13,13 @@ export default async function userHandler(req: NextApiRequest, res: NextApiRespo
     axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;   
     
     switch (method) {
+      case "GET": {
+        const response = await axios.get(
+          `http://localhost:4000/account/${accountId}`
+        );
+        res.status(200).json({ account: response.data , isSuccess: true })
+        break
+      }
       case "PUT": {
         const { body } = req;
         const response = await axios.put(
@@ -21,14 +28,15 @@ export default async function userHandler(req: NextApiRequest, res: NextApiRespo
             body
           }
         );
-        res.status(200).json({ data: response, isSuccess: true })
+        res.status(200).json({ account: response.data, isSuccess: true })
         break
       }
       case "DELETE": {
         await axios.delete(
           `http://localhost:4000/account/${accountId}`);
         res.status(200).json({ isSuccess: true })
-        break}
+        break
+      }
       default:
         res.setHeader("Allow", ["PUT", "DELETE"])
         res.status(405).end(`Method ${method} Not Allowed`)
