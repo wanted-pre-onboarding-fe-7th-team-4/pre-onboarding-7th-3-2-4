ad26a2cdc3b3bc9d40094e98ac4e4896e91fb6f5
A <movie3342@gmail.com>
2022-11-19 00:22:51 +0900

    generateQueryString 함수 개선

--- preface/lib/utils/generateQueryString.ts
+++ preface/lib/utils/generateQueryString.ts
@@ -1,13 +1,9 @@
-import { Queries, QueryKeys } from "lib/types/types";
+import { Queries } from "lib/types/types";
 
 export const generateQueryString = (query: Queries) => {
-  const queryList: string[] = [];
-  let key: QueryKeys | undefined;
-  for (key in query) {
-    const queryKey = key;
-    if (queryKey && query[queryKey]) {
-      queryList.push(`${queryKey}=${query[queryKey]}`);
-    }
-  }
-  return queryList.length !== 0 ? `?${queryList.join("&")}` : "";
+  const queryEntries = Object.entries(query);
+  const queryStrings = queryEntries
+    .map(([key, value]) => value && `${key}=${value}`)
+    .filter((value) => value !== undefined);
+  return `?${queryStrings.join("&")}`;
 };
